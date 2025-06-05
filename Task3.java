62c15267b2796d4e0f6f5160dbad9824aa50a810
Joshini2005731 <2322jb18@kgcas.com>
2025-06-05 14:52:20 +0530

    Create Task3.java

---
+++ Task3.java
@@ -0,0 +1,15 @@
+package basics;
+
+public class interst {
+
+	public static void main(String[] args) {
+		int principal=1000;
+		int rate=5;
+		int time=2;
+		int mul=principal*rate*time;
+		int div=mul/100;
+		System.out.println("your intrest is:");
+		System.out.println(div);
+	}
+
+}
